be4eb01044353287789a471f7840098182316309
Dmitry Dorofeev <dmitry@MacBook-Air-Dmitry.local>
2015-03-24 13:28:14 +0300

    pre-test functions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,9 @@
 __author__ = 'dmitry'
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import os
 
 
@@ -8,16 +11,15 @@ class PageObject():
     def __init__(self):
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.FIREFOX
+            desired_capabilities=getattr(DesiredCapabilities, os.environ.get('TTHA2BROWSER'))
         )
-        self.driver.get("http://ftest.stud.tech-mail.ru/")
+        self.driver.get('http://ftest.stud.tech-mail.ru/')
 
     def show_login_form(self):
         show_button = self.driver.find_element_by_css_selector('.login-button>a')
         show_button.click()
 
     def login(self, login):
-
         self.show_login_form()
 
         login_field = self.driver.find_element_by_css_selector('input[name=login]')
@@ -27,8 +29,17 @@ class PageObject():
         login_field.send_keys(login)
         password_field.send_keys(os.environ.get('TTHA2PASSWORD'))
         form.submit()
-
-        self.driver.save_screenshot('screenie.png')
+        wait = WebDriverWait(self.driver, 10)
+        user = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'username')))
+        return user.is_displayed()
 
     def close(self):
         self.driver.close()
+
+    def open_create_topic(self):
+        button = self.driver.find_element_by_id('modal_write_show')
+        button.click()
+        wait = WebDriverWait(self.driver, 10)
+        button_write = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '.write-item-type-topic>.write-item-link')))
+        button_write.click()
--- tests/test_post.py
+++ tests/test_post.py
@@ -5,7 +5,12 @@ from pages.main_page import PageObject as MainPage
 
 
 class PostTestCase(unittest.TestCase):
-    def test_post_create(self):
-        page = MainPage()
-        page.login('ftest2@tech-mail.ru')
-        page.close()
+    def setUp(self):
+        self.page = MainPage()
+        self.page.login('ftest2@tech-mail.ru')
+
+    def tearDown(self):
+        self.page.close()
+
+    def test_open_edit(self):
+        self.page.open_create_topic()
